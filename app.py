1d62206b2a71e1940f3ea9ad4b2b47ecf806bda7
Wertzhog <ljupko.jurcevic@gmail.com>
2023-04-20 18:50:28 +0100

    Edit food item functionality

--- app.py
+++ app.py
@@ -10,7 +10,16 @@ def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+#Get specific post function
 
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM FoodFacts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 @app.route("/")
 def home():
@@ -39,7 +48,31 @@ def create():
             return "A database error occurred."
     return render_template('create.html')
 
-
+#Edit food item
+@app.route('/edit/<id>/', methods=('GET', 'POST'))
+def edit(id):
+    foods_sel = get_post(id)
+    if request.method == 'POST':
+        try:
+            food = request.form['food']
+            calories = request.form['calories']
+            carbs = request.form['carbs']
+            protein = request.form['protein']
+            fat = request.form['fat']
+            id_f = request.form['id']
+            conn = get_db_connection()
+            conn.execute('UPDATE FoodFacts SET food = ?, calories = ?, protein = ?, carbs = ?, fat = ?'
+                         ' WHERE id = ?',
+                         (food, calories, protein, carbs, fat, id_f))
+            conn.commit()
+            conn.close()
+            flash("Food item was edited successfully.")
+            return redirect(url_for('food'))
+        except sqlite3.Error as e:
+            print(e)
+            return "A database error occurred."
+    
+    return render_template('edit_food.html', foods_sel=foods_sel)
 
 
 
